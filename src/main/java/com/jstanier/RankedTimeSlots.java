77d3b57bc200a63a006506191945c41024b2c1e2
James Stanier <jamiestanier@gmail.com>
2014-04-06 15:06:34 +0100

    RankedTimeSlots class

---
+++ src/main/java/com/jstanier/RankedTimeSlots.java
@@ -0,0 +1,36 @@
+package com.jstanier;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RankedTimeSlots {
+    
+    @Autowired
+    private RankedDaysOfWeek rankedDaysOfWeek;
+    
+    @Autowired
+    private RankedTimesOfWeek rankedTimesOfWeek;
+    
+    private List<TimeSlot> rankedTimeSlots;
+
+    @PostConstruct
+    private void generateRankedTimeSlots() {
+        rankedTimeSlots = Lists.newArrayList();
+        List<Integer> timesOfWeek = rankedTimesOfWeek.getRankedTimesOfWeek();
+        List<Integer> daysOfWeek = rankedDaysOfWeek.getRankedDaysOfWeek();
+        for (Integer timeOfWeek : timesOfWeek) {
+            for(Integer dayOfWeek : daysOfWeek) {
+                TimeSlot timeslot = new TimeSlot(timeOfWeek, dayOfWeek);
+                rankedTimeSlots.add(timeslot);
+            }
+        }
+    }
+    
+    public List<TimeSlot> getRankedTimeSlots() {
+        return rankedTimeSlots;
+    }
+}
