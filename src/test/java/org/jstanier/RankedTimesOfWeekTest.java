60cc75bc2fe56a31d8373efb9131ce1589a9905d
James Stanier <jamiestanier@gmail.com>
2014-04-06 15:06:19 +0100

    RankedTimesOfWeek class

---
+++ src/test/java/org/jstanier/RankedTimesOfWeekTest.java
@@ -0,0 +1,24 @@
+package org.jstanier;
+
+import com.google.common.collect.ImmutableList;
+import com.jstanier.RankedTimesOfWeek;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RankedTimesOfWeekTest {
+    
+    @InjectMocks
+    private RankedTimesOfWeek rankedTimesOfWeek;
+    
+    @Test
+    public void rankedTimesOfWeek_containsTimesInExpectedOrder() {
+        ImmutableList<Integer> expectedTimes = ImmutableList.of(14,20,21,17,9,8,15,23,0);
+        List<Integer> rankedTimes = rankedTimesOfWeek.getRankedTimesOfWeek();
+        Assert.assertEquals(expectedTimes, rankedTimes);
+    }
+}
