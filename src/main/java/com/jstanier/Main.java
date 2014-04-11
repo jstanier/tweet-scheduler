6d9d48ef0e4b86ba2511c9a357d5e341d90049cd
James Stanier <jamess@brandwatch.com>
2014-04-11 11:49:41 +0100

    Add input from file support

--- src/main/java/com/jstanier/Main.java
+++ src/main/java/com/jstanier/Main.java
@@ -9,13 +9,28 @@ public class Main implements Closeable {
 
     private AnnotationConfigApplicationContext applicationContext;
 
-    public Main() {
+    public Main(String pathToCsvFile) {
         applicationContext = new AnnotationConfigApplicationContext(Config.class);
         applicationContext.registerShutdownHook();
+        runScheduler(pathToCsvFile);
     }
 
+    private void runScheduler(String pathToCsvFile) {
+        InputRunner inputRunner = applicationContext.getBean(InputRunner.class);
+        inputRunner.run(pathToCsvFile);
+    }
+
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        Main main = new Main();
+        if (args.length != 1) {
+            exitWithError("Expected a path to a CSV file as argument.");
+        }
+        new Main(args[0]);
+    }
+
+    private static void exitWithError(String error) {
+        System.err.println(error);
+        System.exit(1);
     }
 
     public void close() throws IOException {
